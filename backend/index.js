7f195c403ddef107bf234679ddd2a5ed9a1d92c2
kartikneelagund <kartikneelgund028@gmail.com>
2025-08-28 20:14:23 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const connection = require("./controllers/authController"); // MongoDB connection
+const userRoutes = require("./routes/authRoutes");           // /api/users (for signup/login)
+const authRoutes = require("./middleware/auth");             // auth middleware (if needed)
+const userCrudRoutes = require("./routes/userCrudRoutes");   // /api/userscrud (CRUD ops)
+
+const app = express();
+
+// Connect to MongoDB
+connection();
+
+//  Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/users", userRoutes);        // e.g., POST /api/users/register
+app.use("/api/auth", authRoutes);         // middleware if used
+app.use("/api/userscrud", userCrudRoutes); // e.g., GET, PUT, DELETE user data
+
+app.get('/',(req,res)=>{
+    res.send({
+        activeStatus:true,
+        error:false,
+    })
+})
+// Start Server
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
---
+++ backend/models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
+
+const userSchema = new mongoose.Schema({
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
+	email: { type: String, required: true },
+	password: { type: String, required: true },
+	date: {
+  type: Date,
+  default: Date.now,
+},
+  status:  { type: String, default: "new" }, 
+  status: { type: String, default: "active" }, 
+
+
+});
+
+userSchema.methods.generateAuthToken = function () {
+	const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
+		expiresIn: "7d"});
+	return token;
+};
+
+const User = mongoose.model("user", userSchema);
+
+const validate = (data) => {
+	const schema = Joi.object({
+		firstName: Joi.string().required().label("First Name"),
+		lastName: Joi.string().required().label("Last Name"),
+		email: Joi.string().email().required().label("Email"),
+		password: passwordComplexity().required().label("Password"),
+	});
+	return schema.validate(data);
+};
+
+module.exports = mongoose.model("User", userSchema);
+module.exports = { User, validate };
