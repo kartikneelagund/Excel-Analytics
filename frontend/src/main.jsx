0e9593d28f96408d2763d0612f78b0ba42129424
kartikneelagund <kartikneelgund028@gmail.com>
2025-08-28 21:54:17 +0530

    7th commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,16 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import './index.css'
-import ReactDOM from 'react-dom/client'
+import './index.css';
 import App from './App.jsx';
-import { SidebarProvider } from '../src/components/Dashbord/context/sidebarContext.jsx';
-
+import { SidebarProvider } from './components/Dashbord/context/sidebarContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-  <SidebarProvider>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </SidebarProvider>
- 
-
-)
+  <React.StrictMode>
+    <SidebarProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SidebarProvider>
+  </React.StrictMode>
+);
